21b196731591c32cc0eb9c465947ce6f86088d75
Saulo Santos <prof.saulorenato@gmail.com>
2024-08-09 00:18:21 -0300

    feat: create hook useFetch

---
+++ src/hooks/useFetch.ts
@@ -0,0 +1,54 @@
+import { useEffect, useState } from "react";
+
+export function useFetch() {
+  const [data, setData] = useState<unknown[]>([]);
+  const [page, setPage] = useState<number>(1);
+  // const [totalPage, setTotalPage] = useState<number>(0);
+  const [loading, setLoading] = useState(true);
+  const itemsPerPage = 8;
+
+  function loadData() {
+    setLoading(true);
+    fetch(
+      `https://hp-api.onrender.com/api/characters?_page=${page}&_limit=${itemsPerPage}`,
+      {
+        method: "GET",
+      }
+    )
+    .then(async (response) => {
+      // setTotalPage(
+      //   Number(response.headers.get("X-Total-Count")) / itemsPerPage
+      // );
+      const json = await response.json();
+      setData([...data, ...json]);
+    })
+    .catch((err) => console.log("ERROR: ", err))
+    .finally(() => setLoading(false));
+}
+
+function handleScroll() {
+  if (
+    window.innerHeight + document.documentElement.scrollTop <
+      document.documentElement.offsetHeight ||
+    // page === totalPage ||
+    loading
+  ) {
+    return;
+  }
+
+  setPage(page + 1);
+}
+
+useEffect(() => {
+  loadData();
+// eslint-disable-next-line react-hooks/exhaustive-deps
+}, [page]);
+
+useEffect(() => {
+  window.addEventListener("scroll", handleScroll);
+  return () => window.removeEventListener("scroll", handleScroll);
+// eslint-disable-next-line react-hooks/exhaustive-deps
+}, [loading]);
+
+return { loading, data, page };
+}
